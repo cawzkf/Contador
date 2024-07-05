91b8ea2a178208044a1209d36585e5548a22c102
cawzkf <camilafelixdosreis@gmail.com>
2024-07-05 15:42:31 -0400

    Time númerico e novo button

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         val buttonStart = findViewById<Button>(R.id.buttonStart)
 
+        buttonStart.setOnClickListener {
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
