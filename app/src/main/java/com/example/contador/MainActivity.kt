b7b57304b4b8b6a18363a9b70acd74d0530a7d97
cawzkf <camilafelixdosreis@gmail.com>
2024-07-05 16:49:48 -0400

    Funções startTimer e timer Runnable

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -3,30 +3,59 @@ package com.example.contador
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.os.Handler
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputLayout.LengthCounter
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textViewCounter: TextView
-    private var counter = 0
+    private lateinit var handler: Handler
+    private var startTime: Long = 0
+    private var isRunning: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val buttonStart = findViewById<Button>(R.id.buttonStart)
+        textViewCounter = findViewById(R.id.textViewCounter)
+        handler = Handler()
+        isRunning = false
 
-        buttonStart.setOnClickListener {
-
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 
-}
+        fun startTimer(view: View){
+            if(!isRunning){
+                startTime = System.currentTimeMillis()
+                handler.postDelayed(timerRunnable,0)
+                isRunning = true
+            }
+        }
+
+        private val timerRunnable = object : Runnable {
+            override fun run(){
+                val millis = System.currentTimeMillis() - startTime
+                var seconds = (millis/1000).toInt()
+                val minutes = seconds/60
+                val hours = minutes/60
+
+                seconds %=60
+
+                val time =String.format("%02d:%02d:%02d", hours, minutes % 60, seconds)
+                textViewCounter.text = time
+
+                handler.postDelayed(this,500)
+            }
+        }
+    }
